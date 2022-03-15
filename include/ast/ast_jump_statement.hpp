8220f23c7eb69e210a947525ffbf7fbd19c8f190
Václav Pavlíček <vaclav@pavlicek.cz>
2022-03-15 09:43:13 +0000

    feat(codegen): add code generation for the return statement

--- include/ast/ast_jump_statement.hpp
+++ include/ast/ast_jump_statement.hpp
@@ -10,6 +10,21 @@ public:
     Return(Node* _expression): expression(_expression) {
         this->type = "ReturnExpression";
     }
+
+    virtual std::string compileToMIPS() const {
+        std::string result = "";
+
+        if (expression->type.compare("Variable") == 0) {
+            Variable *variable = (Variable*) expression;
+            int registerNumber = RegisterAllocator::getRegisterNumberForVariable(variable->getName());
+            result = "addu $2, $" + std::to_string(registerNumber) + ", $0";
+        } else if (expression->type.compare("Constant") == 0) {
+            Constant *constant = (Constant*) expression;
+            result = "li $2, " + RegisterAllocator::intToHex(constant->getValue());
+        }
+
+        return result;
+    }
 };
 
 #endif
